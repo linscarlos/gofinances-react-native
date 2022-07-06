e7f993364ea53afc3be0ae069b251626c52662d8
Carlos Lins <carloslinsdev@gmail.com>
2022-07-06 19:51:32 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme'
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+
+} from '@expo-google-fonts/poppins'
+
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
+
